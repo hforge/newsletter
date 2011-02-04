000f2259e899cd6292ffe34531f0c8d6073021fa
David Versmisse <david.versmisse@itaapy.com>
2011-02-04 10:01:58 +0100

    Update the copyright

--- html_data_views.py
+++ html_data_views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2007 Taverne Sylvain <taverne.sylvain@gmail.com>
+# Copyright (C) 2010 David Versmisse <david.versmisse@itaapy.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
